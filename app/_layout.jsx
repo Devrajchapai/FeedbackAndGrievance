94fa7cac74083fc29adef65a6a79e57056559987
Cosmic <50874542+CosmicGautam@users.noreply.github.com>
2025-08-19 12:43:32 +0545

    Token saved

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -9,25 +9,69 @@ import Madhesh from '../screens/Madhesh';
 import Lumbini from '../screens/Lumbini';
 import Karnali from '../screens/Karnali';
 import Sudurpashchim from '../screens/Sudurpashchim';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
 
 const layout = () => {
+   const [loading, setLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     const Stack = createNativeStackNavigator();
+
+    useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem("accessToken");
+
+        if (token) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (err) {
+        console.log("Error checking token", err);
+        setIsLoggedIn(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="home" component={HomeScreen} options={{headerShown : false}}/>
-
-      <Stack.Screen name="Bagmati" component ={Bagmati} options={{headerShown : false}}/>
-      <Stack.Screen name="Gandaki" component ={Gandaki} options={{headerShown : false}}/>
-      <Stack.Screen name="Koshi" component ={Koshi} options={{headerShown : false}}/>
-      <Stack.Screen name="Madhesh" component ={Madhesh} options={{headerShown : false}}/>
-      <Stack.Screen name="Lumbini" component ={Lumbini} options={{headerShown : false}}/>
-      <Stack.Screen name="Karnali" component ={Karnali} options={{headerShown : false}}/>
-      <Stack.Screen name="Sudurpashchim" component ={Sudurpashchim} options={{headerShown : false}}/>
-
-
-      <Stack.Screen name="login" component={LoginScreen} options={{headerShown : false}}/>
-      <Stack.Screen name="signup" component={SignupScreen} options={{headerShown : false}}/>
-   </Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isLoggedIn ? (
+        <>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Bagmati" component ={Bagmati} />
+        <Stack.Screen name="Gandaki" component ={Gandaki} />
+        <Stack.Screen name="Koshi" component ={Koshi} />
+        <Stack.Screen name="Madhesh" component ={Madhesh} />
+        <Stack.Screen name="Lumbini" component ={Lumbini} />
+        <Stack.Screen name="Karnali" component ={Karnali} />
+        <Stack.Screen name="Sudurpashchim" component ={Sudurpashchim} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </>
+      )}
+    </Stack.Navigator>
   )
 }
 
